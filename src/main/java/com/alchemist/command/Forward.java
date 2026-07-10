3ef5113e05a9c8d9c3bf32fcc63cec66dd0bca2e
Mapaseka Moloi <mapmoljhb025@student.wethinkcode.co.za>
2026-07-10 12:48:59 +0200

    Start code

---
+++ src/main/java/com/alchemist/command/Forward.java
@@ -0,0 +1,17 @@
+package com.alchemist.command;
+
+public class Forward extends Command {
+    public Forward(String name, String argument) {
+        super(name, argument);
+    }
+
+    @Override
+    public Command execute(String name, String argument) {
+        int arg = Integer.parseInt(argument);
+
+        for(int i = 0; i <= arg; i++) {
+
+
+        }
+    }
+}
---
+++ src/main/java/com/alchemist/world/Morality.java
@@ -0,0 +1,5 @@
+package com.alchemist.world;
+
+public interface Morality {
+
+}
